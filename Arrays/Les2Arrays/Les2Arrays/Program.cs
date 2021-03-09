17ccc2e4def8a0d6eebf56f96b8a546807793111
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-03-09 13:30:08 +0100

    Added Arrays Achievment

---
+++ Arrays/Les2Arrays/Les2Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Les2Arrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] Array1 = { 10, 20, 30, 40, 50, 60 };
+			int[] Array2 = { };
+
+			for (int i = 0; i < Array2.Length; i++)
+			{
+				int s = Array2[i];
+				Console.WriteLine(s);
+			}
+
+			for (int i = Array1.Length - 1; i >= 0; i--)
+			{
+				int s = Array1[i];
+				Console.WriteLine(s);
+			}
+		}
+	}
+}
