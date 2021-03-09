8ba7dee65bcca84ba465516170df03389727059b
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-03-09 14:31:03 +0100

    Added Les2Loops (gold)

---
+++ Les2VariablesEnDatatypes/Les2VariablesAndDatatypes/Les2VariablesAndDatatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Les2VariablesAndDatatypes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//brons
+			Console.WriteLine("Bronze achievment : ");
+			int var1 = 1;
+			float float1 = 0.1f;
+			bool bool1 = true;
+			//Console.WriteLine(var1, float1, bool1);
+		}
+	}
+}
---
+++ les2ArraysAndLoops/Les2ArraysAndLoops/Les2ArraysAndLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Les2ArraysAndLoops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//loops gold achievment
+			int[] numbers = { 0, 2, 4, 6, 8, 10 };
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Console.WriteLine(numbers[i]);
+			}
+		}
+	}
+}
