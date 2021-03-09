7238e1df131c35f090f1c47634c4947c5f2ad53a
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-03-09 13:20:57 +0100

    Added P1 project

---
+++ P1/Project1/Project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.Clear();
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
